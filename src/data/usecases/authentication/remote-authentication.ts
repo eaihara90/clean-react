68eeefe135219a299d814505cfa73cdfc5111041
Eduardo Aihara <eaihara90@gmail.com>
2023-10-27 14:37:21 -0300

    feat: ensure RemoteAuthentication class HttpPostClient with correct URL

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,12 @@
+import { IHttpPostClient } from 'data/protocols/http/http-post-client';
+
+export class RemoteAuthentication {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: IHttpPostClient
+  ) { }
+
+  public async auth(): Promise<void> {
+    await this.httpPostClient.post(this.url);
+  }
+}
