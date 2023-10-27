edf267c2ce481e65ec99dd86020b49d1d0e76cac
Eduardo Aihara <eaihara90@gmail.com>
2023-10-27 13:55:33 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,10 @@
+import { AccountModel } from 'domain/models/account-model';
+
+export interface Authentication {
+  auth(params: AuthenticationParams): Promise<AccountModel>;
+}
+
+type AuthenticationParams = {
+  email: string;
+  password: string;
+}
